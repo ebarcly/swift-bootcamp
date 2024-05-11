1bef595f710b9954993f1e5fea2e23654e8f7b78
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2024-05-10 23:07:03 -0400

    timer and sounds added

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -6,39 +6,56 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
-import SwiftUI
+import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var timer: Timer?
+    var player: AVAudioPlayer?
     var count = 0
-    
-    @IBAction func hardnessSelected(_ sender: UIButton) {
-        
-        let hardness: String? =
-        sender.currentTitle!
-        
-        
-        print(eggTimes[hardness!]!)
-        
-    }
     
     @IBOutlet weak var countDownLabel: UILabel!
     
-    var timer: Timer?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func hardnessSelected(_ sender: UIButton) {
         
+        guard let hardness = sender.currentTitle, let duration = eggTimes[hardness] else { return }
+        count = duration * 60 // Conver minutes to seconds if needed
+        countDownLabel.text = formatTime(seconds: count)
+        timer?.invalidate() // Stop any existing timer
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCountDown), userInfo: nil, repeats: true)
     }
     
     @objc func updateCountDown() {
         if count > 0 {
-            countDownLabel.text = String(count-1)
+            count -= 1
+            countDownLabel.text = formatTime(seconds: count)
         } else {
             timer?.invalidate()
-            // Do something when the countdown is over
+            playSound()
+            countDownLabel.text = "Done!"
         }
+    }
+    
+    func formatTime (seconds: Int) -> String {
+        let minutes = seconds / 60
+        let seconds = seconds % 60
+        return String(format: "%d:%02d", minutes, seconds)
+    }
+    
+    func playSound () {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")!
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print("Could not load sound file.")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countDownLabel.text = "Select Hardness" // Initial text for the label
     }
 }
