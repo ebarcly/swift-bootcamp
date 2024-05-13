00a5cebb4945dfeaabbacf483bf382696af7fcf2
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2024-05-12 20:53:36 -0400

    finished Egg App

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -11,30 +11,42 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 1, "Medium": 7, "Hard": 12]
     var timer: Timer?
     var player: AVAudioPlayer?
     var count = 0
+    var totalTime = 0
     
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var countDownLabel: UILabel!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         guard let hardness = sender.currentTitle, let duration = eggTimes[hardness] else { return }
-        count = duration * 60 // Conver minutes to seconds if needed
+        totalTime = duration * 60 // Set the total time
+        count = totalTime // Initialize count with total time
         countDownLabel.text = formatTime(seconds: count)
-        timer?.invalidate() // Stop any existing timer
+        progressBar.progress = 0.0 // Reset progress bar
+        timer?.invalidate() // Stop any existing timer to prevent timer override
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCountDown), userInfo: nil, repeats: true)
+        
+        
     }
     
+    
+    // Use @objc to call this function for some reason...
     @objc func updateCountDown() {
         if count > 0 {
             count -= 1
+            let progressFraction = Float(count) / Float(totalTime) // Calculate the fraction of the time left
+            progressBar.setProgress(1.0 - progressFraction, animated: true)  // Update the progress bar
             countDownLabel.text = formatTime(seconds: count)
         } else {
             timer?.invalidate()
+            progressBar.setProgress(1.0, animated: true)  // Ensure progress bar completes
             playSound()
-            countDownLabel.text = "Done!"
+            mainLabel.text = "DONE!"
         }
     }
     
@@ -57,5 +69,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         countDownLabel.text = "Select Hardness" // Initial text for the label
+        progressBar.progress = 0.0 // Initial state of the progress bar
     }
 }
