f427bb86425e2c74a18c447e0b998856359450af
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2024-12-25 18:28:26 -0500

    Finishing touches: UI and UX Improvements

--- FlashChat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ FlashChat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -8,20 +8,87 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class ChatViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = "⚡️FlashChat"
+        
+        tableView.delegate = self
+        
+        tableView.dataSource = self
+        
+        // Add title to Screen
+        navigationItem.title = K.appName
+        
+        // hides the back button
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
+        loadMessages()
         
     }
     
+    func loadMessages() {
+        // Create a reference to the collection of messages
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, erorr) in
+                
+                // Empty messages inside closure
+                self.messages = []
+                
+                if let e = erorr {
+                    print("Error retrieving data. \(e)")
+                } else {
+                    if let snapshotDocuments = querySnapshot?.documents {
+                        for doc in snapshotDocuments {
+                            let data  = doc.data()
+                            if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                                let newMessage = Message(sender: messageSender, body: messageBody)
+                                self.messages.append(newMessage)
+                                
+                                
+                                // Reload the table view with the new data and scroll to the bottom
+                                DispatchQueue.main.async {
+                                    self.tableView.reloadData()
+                                    let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                    self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+    }
+    
     @IBAction func sendPressed(_ sender: UIButton) {
+        // Create a new message from the text field
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email  {
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField : messageSender,
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField : Date().timeIntervalSince1970]) { (error) in
+                    if let e = error {
+                        print("Error, \(e)")
+                    } else {
+                        print("Data has been saved")
+                        
+                        DispatchQueue.main.async {
+                            self.messageTextfield.text = ""
+                        }
+                    }
+                }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -37,5 +104,54 @@ class ChatViewController: UIViewController {
         }
         
     }
+}
+
+// MARK: - TableViewDataSource
+extension ChatViewController: UITableViewDataSource {
+    // Returns the number of rows in the table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    // Returns the cell that will be displayed in the table
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        as! MessageCell
+        cell.label.text = message.body
+        
+        // current user message
+        if message.sender == Auth.auth().currentUser?.email {
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        }
+        
+        // Another sender message
+        else {
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
+        return cell
+    }
     
+}
+// MARK: - TableViewDelegate
+// Allows the user to select a row
+extension ChatViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+
+// MARK: - TextFieldDelegate
+// Hides the keyboard when the return key is pressed
+extension ChatViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        messageTextfield.endEditing(true)
+        return true
+    }
 }
--- FlashChat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ FlashChat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,15 +9,32 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = "⚡️FlashChat"
-       
+        titleLabel.text = ""
+        
+        UIView.animate(withDuration: 2.5, animations: {
+            self.titleLabel.text = K.appName
+        })
+        
+        
     }
     
-
+    
 }
